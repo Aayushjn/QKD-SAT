172df5673a250646a8ffd3b97502f863fcd4b033
Aayush Jain <aayushjn11@gmail.com>
2024-09-17 17:23:53 +0530

    update driver code

--- qkd/simulation.py
+++ qkd/simulation.py
@@ -1,5 +1,3 @@
-from itertools import pairwise
-
 import matplotlib.cm as cm
 import matplotlib.colors as colors
 import matplotlib.pyplot as plt
@@ -7,26 +5,25 @@ import networkx as nx
 import numpy as np
 from network import Network
 
-n = 10
 
 np.set_printoptions(linewidth=120)
 
-net_graph = Network.random(n)
-print(net_graph.disjoint_paths)
-# print(net_graph.optimality())
+num_nodes = 10
+net_graph = Network.random(num_nodes)
+print(net_graph.reduced_paths)
 
-fig, axes = plt.subplots(2, 2)
+fig, axes = plt.subplots(1, 2)
 # pos = nx.bfs_layout(network_graph, 0)
 pos = nx.arf_layout(net_graph)
 
 node_color = ["lightgreen" if i == 0 or i == net_graph.number_of_nodes() - 1 else "lightblue" for i in net_graph.nodes]
 node_size = 1000 * net_graph.curiosity_matrix
 nx.draw_networkx_nodes(
-    net_graph, pos=pos, ax=axes[0, 0], nodelist=net_graph.nodes, node_color=node_color, node_size=node_size
+    net_graph, pos=pos, ax=axes[0], nodelist=net_graph.nodes, node_color=node_color, node_size=node_size
 )
-nx.draw_networkx_edges(net_graph, pos=pos, ax=axes[0, 0], edgelist=net_graph.edges, edge_color="gray")
-nx.draw_networkx_labels(net_graph, pos=pos, ax=axes[0, 0], font_size=10)
-axes[0, 0].set_title("Network Graph")
+nx.draw_networkx_edges(net_graph, pos=pos, ax=axes[0], edgelist=net_graph.edges, edge_color="gray")
+nx.draw_networkx_labels(net_graph, pos=pos, ax=axes[0], font_size=10)
+axes[0].set_title("Network Graph")
 
 edge_list = [
     (i, j)
@@ -39,7 +36,7 @@ norm = colors.Normalize(vmin=0.0, vmax=1.0)
 nx.draw_networkx_nodes(
     net_graph,
     pos=pos,
-    ax=axes[0, 1],
+    ax=axes[1],
     nodelist=range(1, net_graph.number_of_nodes() - 1),
     node_color="lightblue",
     node_size=500,
@@ -47,7 +44,7 @@ nx.draw_networkx_nodes(
 nx.draw_networkx_edges(
     net_graph,
     pos=pos,
-    ax=axes[0, 1],
+    ax=axes[1],
     edgelist=edge_list,
     edge_color=tuple(map(lambda t: cmap(norm(net_graph.collaboration_matrix[t[0]][t[1]])), edge_list)),
     width=tuple(map(lambda t: net_graph.collaboration_matrix[t[0]][t[1]] * 8, edge_list)),
@@ -56,41 +53,41 @@ nx.draw_networkx_edges(
 nx.draw_networkx_labels(
     net_graph,
     pos=pos,
-    ax=axes[0, 1],
+    ax=axes[1],
     labels={i: str(i) for i in range(1, net_graph.number_of_nodes() - 1)},
     font_size=10,
 )
-axes[0, 1].set_title("Collaboration Graph")
+axes[1].set_title("Collaboration Graph")
 
-edge_list = {}
-color_values = list(colors.TABLEAU_COLORS.keys())
-for i, path in enumerate(net_graph.disjoint_paths):
-    color = color_values[i % len(color_values)]
-    for edge in pairwise(path):
-        edge_list[edge] = color
-node_list = {edge[0] for edge in edge_list}
-node_list.add(net_graph.number_of_nodes() - 1)
-node_color = ["lightgreen" if i == 0 or i == net_graph.number_of_nodes() - 1 else "lightblue" for i in node_list]
-node_size = [1000 * net_graph.curiosity_matrix[i] for i in node_list]
-nx.draw_networkx_nodes(
-    net_graph, pos=pos, ax=axes[1, 0], nodelist=node_list, node_color=node_color, node_size=node_size
-)
-nx.draw_networkx_edges(
-    net_graph, pos=pos, ax=axes[1, 0], edgelist=edge_list.keys(), edge_color=edge_list.values(), width=2
-)
-nx.draw_networkx_labels(net_graph, pos=pos, ax=axes[1, 0], labels={node: str(node) for node in node_list}, font_size=10)
-axes[1, 0].set_title("Disjoint Paths")
+# edge_list = {}
+# color_values = list(colors.TABLEAU_COLORS.keys())
+# for i, path in enumerate(net_graph.reduced_paths):
+#     color = color_values[i % len(color_values)]
+#     for edge in pairwise(path):
+#         edge_list[edge] = color
+# node_list = {edge[0] for edge in edge_list}
+# node_list.add(net_graph.number_of_nodes() - 1)
+# node_color = ["lightgreen" if i == 0 or i == net_graph.number_of_nodes() - 1 else "lightblue" for i in node_list]
+# node_size = [1000 * net_graph.curiosity_matrix[i] for i in node_list]
+# nx.draw_networkx_nodes(
+#     net_graph, pos=pos, ax=axes[1, 0], nodelist=node_list, node_color=node_color, node_size=node_size
+# )
+# nx.draw_networkx_edges(
+#     net_graph, pos=pos, ax=axes[1, 0], edgelist=edge_list.keys(), edge_color=edge_list.values(), width=2
+# )
+# nx.draw_networkx_labels(net_graph, pos=pos, ax=axes[1, 0], labels={node: str(node) for node in node_list}, font_size=10)
+# axes[1, 0].set_title("Disjoint Paths")
 
 
-x_axis = [str(path) for path in net_graph.disjoint_paths]
-y_axis = [path.risk for path in net_graph.disjoint_paths]
-bar = axes[1, 1].bar(
-    x_axis,
-    y_axis,
-    color="lightblue",
-)
-axes[1, 1].bar_label(bar, fmt="%.2f")
-axes[1, 1].set_title("Path Risks")
+# x_axis = [str(path) for path in net_graph.reduced_paths]
+# y_axis = [path.risk for path in net_graph.reduced_paths]
+# bar = axes[1, 1].bar(
+#     x_axis,
+#     y_axis,
+#     color="lightblue",
+# )
+# axes[1, 1].bar_label(bar, fmt="%.2f")
+# axes[1, 1].set_title("Path Risks")
 
 
 fig.tight_layout()
